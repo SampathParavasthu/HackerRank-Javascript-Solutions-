dc99d86a415d5489220dfb7316a874838cfeb903
Sampath Paravasthu <write2sampath@gmail.com>
2018-11-26 01:53:16 +0530

    Update designer_pdf_viewer.js

--- algorithms/designer_pdf_viewer.js
+++ algorithms/designer_pdf_viewer.js
@@ -1,24 +1,3 @@
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
-
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
-
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
-
-/////////////// ignore above this line ////////////////////
 function designerPdfViewer(alphabetsHeight,word){
 
   let alphabets = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -30,11 +9,5 @@ function designerPdfViewer(alphabetsHeight,word){
 
 }
 
-
-
-function main() {
-    h = readLine().split(' ');
-    h = h.map(Number);
-    var word = readLine();
-    designerPdfViewer(h,word);
-}
+let result = designerPdfViewer([1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7],'zaba');
+result;
