1a7f704e5f3be5b8632fe0e8b2a8b9e9e36184cf
Sampath Paravasthu <write2sampath@gmail.com>
2018-11-26 01:45:13 +0530

    Update acm_icpc_team.js

--- algorithms/acm_icpc_team.js
+++ algorithms/acm_icpc_team.js
@@ -1,30 +1,4 @@
-'use strict';
 
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-// Complete the acmTeam function below.
 function acmTeam(topic) {
   let maxTopics = 0;
   let maxTeamCount;
@@ -48,25 +22,5 @@ function acmTeam(topic) {
   return [maxTopics, maxTeamCount];
 }
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const nm = readLine().split(' ');
-
-    const n = parseInt(nm[0], 10);
-
-    const m = parseInt(nm[1], 10);
-
-    let topic = [];
-
-    for (let i = 0; i < n; i++) {
-        const topicItem = readLine();
-        topic.push(topicItem);
-    }
-
-    let result = acmTeam(topic);
-
-    ws.write(result.join("\n") + "\n");
-
-    ws.end();
-}
+let result = acmTeam([10101,11100,11010,00101]);
+result ;
