6dc4212250799d3a668423b916822405198d6462
Sampath Paravasthu <write2sampath@gmail.com>
2019-01-04 00:55:47 +0530

    Update matching_same_text_again_and_again.js

--- regex/matching_same_text_again_and_again.js
+++ regex/matching_same_text_again_and_again.js
@@ -1,1 +1,6 @@
 var Regex_Pattern = /([a-z])(\w)(\s)(\W)(\d)(\D)([A-Z])([a-zA-z])([a,e,i,o,u,A,E,I,O,U])(\S)\1\2\3\4\5\6\7\8\9\10/;
+
+/* 
+Alternative Solution : 
+var Regex_Pattern = /([a-z]\w\s\W\d\D[A-Z][a-zA-z][aeiouAEIOU]\S)\1/;
+*/
